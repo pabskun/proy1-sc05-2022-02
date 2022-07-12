a41d112e86624ef04e6a26b81af16b388cb67e08
Pabs Monestel <pablo.monestel@gmail.com>
2022-07-11 20:54:49 -0600

    json de usuarios

---
+++ js/usuarios.js
@@ -0,0 +1,21 @@
+let usuarios = [{
+    'correo': 'leonardo@gmail.com',
+    'nombre': 'Leonardo Herrera',
+    'genero': 'Masculino'
+}, {
+    'correo': 'tamara@gmail.com',
+    'nombre': 'Tamara muñoz',
+    'genero': 'Femenino'
+}, {
+    'correo': 'dereck@gmail.com',
+    'nombre': 'Dereck Rojas',
+    'genero': 'Masculino'
+}, {
+    'correo': 'juan@gmail.com',
+    'nombre': 'Juan Diego Sequeira',
+    'genero': 'Masculino'
+}, {
+    'correo': 'yerelin@gmail.com',
+    'nombre': 'Yerelin Contreras',
+    'genero': 'Femenino'
+}];
